d1d2b57e69b6fe8bb3cfbc980c8611325f920281
Akarm Adjab <akarmadjab@Akarms-MacBook-Pro.local>
2023-07-28 14:33:03 +0100

    Added aria-label to buttons for screen readers

--- src/App.js
+++ src/App.js
@@ -236,6 +236,7 @@ function Comment({
             <div className="buttons">
               {comment.user.username === "juliusomo" && (
                 <button
+                  aria-label="Delete comment button"
                   className="button button-delete"
                   onClick={() =>
                     onDeleteComment(comment.id, Boolean(comment?.replyingTo))
@@ -257,6 +258,7 @@ function Comment({
 
               {comment.user.username === "juliusomo" ? null : (
                 <button
+                  aria-label="Reply to comment button"
                   className="button"
                   onClick={() => setReply((reply) => !reply)}
                 >
@@ -322,7 +324,10 @@ function Score({ comment, score, onUpdateScore }) {
 
   return (
     <div className="comment__score-box">
-      <button onClick={handleUpdateScoreUp}>
+      <button
+        aria-label="Increase the comment score button"
+        onClick={handleUpdateScoreUp}
+      >
         <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
@@ -331,7 +336,10 @@ function Score({ comment, score, onUpdateScore }) {
         </svg>
       </button>
       <p>{updateScore}</p>
-      <button onClick={handleUpdateScoreDown}>
+      <button
+        aria-label="Decrease the comment score button"
+        onClick={handleUpdateScoreDown}
+      >
         <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
@@ -391,7 +399,11 @@ function AddComment({ user, comment, reply, onAddComment, onSetReply }) {
         placeholder="Add comment..."
       />
 
-      <button className="button-filled" onClick={handleSubmitNewComment}>
+      <button
+        aria-label="Add new comment button"
+        className="button-filled"
+        onClick={handleSubmitNewComment}
+      >
         {reply ? "Reply" : "Send"}
       </button>
     </form>
@@ -419,13 +431,13 @@ function AddComment({ user, comment, reply, onAddComment, onSetReply }) {
 function Author() {
   return (
     //  <!-- Author: Akram Adjab -->
-    <div class="attribution">
+    <div className="attribution">
       Challenge by
       <a
         href="https://www.frontendmentor.io?ref=challenge"
         target="_blank"
         rel="noreferrer"
-        class="attribution__link"
+        className="attribution__link"
       >
         Frontend Mentor
       </a>
@@ -434,7 +446,7 @@ function Author() {
         href="https://www.linkedin.com/in/akramadjab/"
         target="_blank"
         rel="noreferrer"
-        class="attribution__link"
+        className="attribution__link"
       >
         Akram Adjab
       </a>
